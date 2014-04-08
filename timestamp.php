f1a257126e0db623d21a2df31b31570a049bed47
shenjia <zshenjia@gmail.com>
2014-04-08 20:51:01 +0800

    init

---
+++ timestamp.php
@@ -0,0 +1,14 @@
+<?php
+array_shift( $argv );
+$query = implode(' ', $argv);
+
+if (!$query) {
+	echo time();
+} else if (is_numeric($query)) {
+	echo date('Y-m-d H:i:s', $query);
+} else if (strtotime($query)) {
+	echo strtotime($query);
+} else {
+	echo 'Failed to parse time! (yyyy-mm-dd hh:mm:ss)';
+}
+echo PHP_EOL;
